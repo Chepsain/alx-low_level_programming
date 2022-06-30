6eb718466f3016d368cdcdc075937f939ceea198
Chepsain <CHEPSAINLANGAT@GMAIL.COM>
2022-06-30 14:33:35 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
+
+
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Success)
+ **/
 
 int main(void)
 
@@ -17,4 +15,5 @@ int main(void)
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
+
 }
