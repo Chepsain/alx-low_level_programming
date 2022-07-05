242604a731b01a7347f262a51cc4a7348edf1f46
Chepsain Bett <104931435+Chepsain@users.noreply.github.com>
2022-07-05 15:51:12 -0700

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,24 +1,41 @@
-#include <stdio.h>
 #include "main.h"
 
+
 /**
- * main - Lists all the natural numbers below 1024 (excluded)
- * that are multiples of 3 or 5.
- *
- * Return: Always 0.
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
 
-int main(void)
+void print_times_table(int n)
 {
-	int i, sum = 0;
+int num, mult, prod;
+if (n >= 0 && n <= 15)
+{
+for (num = 0; num <= n; num++)
+{
+_putchar('0');
+for (mult = 1; mult <= n; mult++)
+{
+_putchar(',');
+_putchar(' ');
 
-	for (i = 0; i < 1024; i++)
-	{
-		if ((i % 3) == 0 || (i % 5) == 0)
-			sum += i;
-	}
-
-	printf("%d\n", sum);
-
-	return (0);
+prod = num * mult;
+if (prod <= 99)
+_putchar(' ');
+if (prod <= 9)
+_putchar(' ');
+if (prod >= 100)
+{
+_putchar((prod / 100) + '0');
+_putchar(((prod / 10)) % 10 + '0');
+}
+else if (prod <= 99 && prod >= 10)
+{
+_putchar((prod / 10) + '0');
+}
+_putchar((prod % 10) + '0');
+}
+_putchar('\n');
+}
+}
 }
