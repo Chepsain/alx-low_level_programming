1b6ba418fae4ca04b66d084380258be9a9fbb5f2
Chepsain <CHEPSAINLANGAT@GMAIL.COM>
2022-07-05 15:23:23 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the times table of the input,
- * starting with 0.
- * @n: The value of the times table to the printed.
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
 
 void print_times_table(int n)
@@ -31,7 +30,7 @@ void print_times_table(int n)
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
+				_putchar(((prod / 10)) % 10 + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
 				{
