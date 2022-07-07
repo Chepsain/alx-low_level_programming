a556e9fa4c5f51d8b4d28880276434ede1343f60
Chepsain <CHEPSAINLANGAT@GMAIL.COM>
2022-07-07 14:31:51 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,10 @@
 #include <stdio.h>
 
 /**
-<<<<<<< HEAD
  * print_numbers - Prints the numbers from 0-9.
-=======
- * print_most_numbers - return  0-9 skips 2 and 4
->>>>>>> 62f2d7c9aaf33d7c818b8839f290ffc8cc8fc5ea
  */
 
-void print_most_numbers(void)
+void print_numbers(void)
 {
 	int i;
 
