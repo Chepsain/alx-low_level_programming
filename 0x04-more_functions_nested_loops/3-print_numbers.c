40bd3e04af34acddf1776476cc7d14e89cd2437e
Chepsain <CHEPSAINLANGAT@GMAIL.COM>
2022-07-07 13:52:13 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - return 0-9
+ */
+
+void print_numbers(void)
+{
+	int 1;
+
+	for (i = '0'; i <= '9' i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
