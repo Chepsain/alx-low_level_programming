d5678c4edb1e627745a418d4f5af836e2894647c
Chepsain <CHEPSAINLANGAT@GMAIL.COM>
2022-07-07 15:44:29 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
- * prints_most_numbers - return 0-9 skips 2 and 4
+ * print_most_numbers - return 0-9 skips 2 and 4
  *
  */
 
@@ -19,25 +18,3 @@ void print_most_numbers(void)
 	}
 	_putchar('\n');
 }
-=======
-#include "main.h"
-
-/**
- * print_most_numbers - prints the numbers, from 0 to 9,
- * except 2 and 4, followed by a new line
- */
-void print_most_numbers(void)
-{
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		if (i != 2 && i != 4)
-		{
-			_putchar(i + '0');
-		}
-	}
-
-	_putchar('\n');
-}
->>>>>>> afb13cdb8822350255639dcd20aa56c5f68daa6f
