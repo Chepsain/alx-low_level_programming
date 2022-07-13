fb92c1e8d494a614e83584883f3830cb0d581b6b
Chepsain Bett <104931435+Chepsain@users.noreply.github.com>
2022-07-13 10:00:06 -0700

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,22 @@
 #include "main.h"
 
 /**
- * _strncpy - cpoies n bytes of src to the dest string
- * @dest: string to copy to
- * @src: string being copied
- * @n: largest number of bytes to copy
+ * strcat - Concatenates the string pointed to by @src, including the terminating
+ *          null byte, to the end of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
  *
- * Return: address of dest
+ * Return: A pointer to the destination string @dest.
  */
-char *_strncpy(char *dest, char *src, int n)
+char *strcat(char *dest, const char *src)
 {
-	int i;
+	int index = 0, dest_len = 0;
 
-	i = 0;
-	while (i < n && *(src + i))
-	{
-		*(dest + i) = *(src + i);
-		i++;
-	}
-	while (i < n)
-	{
-		*(dest + i) = '\0';
-		i++;
-	}
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
