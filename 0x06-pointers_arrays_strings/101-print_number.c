4e54edbaa74c9d1bf0577dedab91167026ec54fb
Chepsain Bett <104931435+Chepsain@users.noreply.github.com>
2022-07-13 11:02:46 -0700

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
 #include "main.h"
 
 /**
@@ -23,7 +19,3 @@ void print_number(int n)
 
 	_putchar((num % 10) + '0');
 }
->>>>>>> b3fb0ab7bab1d87be19c9c423f64c9787dc30995
-=======
-
->>>>>>> 1c46bfd8ec04eed190233cfb94130b02b7d713e7
