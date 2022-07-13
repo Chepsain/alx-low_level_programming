3022e6e832118278e933710872e54df66957b8e1
Chepsain Bett <104931435+Chepsain@users.noreply.github.com>
2022-07-13 10:12:21 -0700

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,17 +1,26 @@
 #include "main.h"
-/**
- * _strcmp - checks if 2 strings are of equal value
- * @s1: destination string
- * @s2: source string to be copied
- *
- * Return: difference of first characters that are of diff value
- */
-int _strcmp(char *s1, char *s2)
-{
-	int j;
 
-	for (j = 0; s1[j] != '\0' && s2[j] != '\0'; j++)
-		if (s1[j] != s2[j])
-			return (s1[j] - s2[j]);
-	return (0);
+/**
+ * _strncpy - Copies at most an inputted number
+ *            of bytes from string src into dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copied from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
 }
