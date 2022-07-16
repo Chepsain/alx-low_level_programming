965c91b527ae9e8254a7037672426445e75e54e7
Chepsain <CHEPSAINLANGAT@GMAIL.COM>
2022-07-16 02:10:59 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,29 +7,29 @@
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int tens, dig, pos = n;
+	double temp_tens = 1;
 
-	if (n < 0)
-	{
-		_putchar(45);
-		m = n * -1;
-	}
+	if (n == 0)
+		_putchar('0');
 	else
 	{
-		m = n;
-	}
+		if (n < 0)
+		{
+		pos = n * -1;
+		_putchar('-');
+		}
+	while (temp_tens <= pos)
+		temp_tens *= 10;
+	tens = temp_tens / 10;
 
-	d = m;
-	count = 1;
-
-	while (d > 9)
+	while (tens >= 1)
 	{
-		d /= 10;
-		count *= 10;
+		dig = pos / tens;
+		_putchar(dig + '0');
+		pos = (pos - (tens * dig));
+		tens /= 10;
 	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
 	}
 }
+
